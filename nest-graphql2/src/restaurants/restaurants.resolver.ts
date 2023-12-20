5e6179270865835a8b90a6e4a77cf95970994a23
cchoseonghun <cchoseonghun@gmail.com>
2023-12-20 12:05:29 +0900

    nest-graphql2: Update restaurant Setup

--- nest-graphql2/src/restaurants/restaurants.resolver.ts
+++ nest-graphql2/src/restaurants/restaurants.resolver.ts
@@ -2,6 +2,7 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { RestaurantsService } from './restaurants.service';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 
 @Resolver(of => Restaurant)
 export class RestaurantsResolver {
@@ -16,6 +17,17 @@ export class RestaurantsResolver {
   async createRestaurant(@Args('input') createRestaurantDto: CreateRestaurantDto): Promise<boolean> {
     try {
       await this.restaurantsService.createRestaurant(createRestaurantDto);
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  }
+
+  @Mutation(returns => Boolean)
+  async updateRestaurant(@Args('input') updateRestaurantDto: UpdateRestaurantDto): Promise<boolean> {
+    try {
+      await this.restaurantsService.updateRestaurant(updateRestaurantDto);
       return true;
     } catch (e) {
       console.log(e);
