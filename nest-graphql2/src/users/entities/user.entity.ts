ea93d79d313549aac84b1e4b24daf474c6ca6461
cchoseonghun <cchoseonghun@gmail.com>
2023-12-21 10:02:46 +0900

    nest-graphql2: Create user account and Handling duplicate error

--- nest-graphql2/src/users/entities/user.entity.ts
+++ nest-graphql2/src/users/entities/user.entity.ts
@@ -1,16 +1,25 @@
+import { InternalServerErrorException } from "@nestjs/common";
+import { Field, InputType, ObjectType, registerEnumType } from "@nestjs/graphql";
 import { CoreEntity } from "src/common/entities/core.entity";
-import { Column, Entity } from "typeorm";
+import { BeforeInsert, Column, Entity } from "typeorm";
+import * as bcrypt from 'bcrypt';
 
-type UserRole = 'client' | 'owner' | 'delivery';
+enum UserRole { Client, Owner, Delivery };
+registerEnumType(UserRole, { name: 'UserRole' });
 
+@InputType({ isAbstract: true })
+@ObjectType()
 @Entity()
 export class User extends CoreEntity {
   @Column()
+  @Field(type => String)
   email: string;
 
   @Column()
+  @Field(type => String)
   password: string;
 
-  @Column()
+  @Column({ type: 'enum', enum: UserRole })
+  @Field(type => UserRole)
   role: UserRole;
 }
