78916320c0ce30cac6fb8381239f7b0f107b2257
cchoseonghun <cchoseonghun@gmail.com>
2023-12-21 10:51:43 +0900

    nest-graphql2: Log In Setup

--- nest-graphql2/src/users/entities/user.entity.ts
+++ nest-graphql2/src/users/entities/user.entity.ts
@@ -3,6 +3,7 @@ import { Field, InputType, ObjectType, registerEnumType } from "@nestjs/graphql"
 import { CoreEntity } from "src/common/entities/core.entity";
 import { BeforeInsert, Column, Entity } from "typeorm";
 import * as bcrypt from 'bcrypt';
+import { IsEmail, IsEnum } from "class-validator";
 
 enum UserRole { Client, Owner, Delivery };
 registerEnumType(UserRole, { name: 'UserRole' });
@@ -13,6 +14,7 @@ registerEnumType(UserRole, { name: 'UserRole' });
 export class User extends CoreEntity {
   @Column()
   @Field(type => String)
+  @IsEmail()
   email: string;
 
   @Column()
@@ -21,12 +23,23 @@ export class User extends CoreEntity {
 
   @Column({ type: 'enum', enum: UserRole })
   @Field(type => UserRole)
+  @IsEnum(UserRole)
   role: UserRole;
 
   @BeforeInsert()
   async hashPassword(): Promise<void> {
     try {
       this.password = await bcrypt.hash(this.password, 10);
+    } catch (e) {
+      console.log(e);
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async checkPassword(aPassword: string): Promise<boolean> {
+    try {
+      const ok = await bcrypt.compare(aPassword, this.password);
+      return ok;
     } catch (e) {
       console.log(e);
       throw new InternalServerErrorException();
--- nest-graphql2/src/users/users.service.ts
+++ nest-graphql2/src/users/users.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
-import { CreateAccountInput } from './dtos/create-account.dto';
+import { CreateAccountInput, CreateAccountOutput } from './dtos/create-account.dto';
+import { LoginInput, LoginOutput } from './dtos/login.dto';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private readonly users: Repository<User>) {}
 
   async createAccount({ email, password, role }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
+  // async createAccount({ email, password, role }: CreateAccountInput): Promise<CreateAccountOutput> {
     try {
       const exists = await this.users.findOne({ where: { email } });
       if (exists) {
@@ -19,5 +21,23 @@ export class UsersService {
     } catch (e) {
       return { ok: false, error: "Couldn't create account" };
     }
+  }
+
+  async login({ email, password }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
+  // async login({ email, password }: LoginInput): Promise<LoginOutput> {
+    try {
+      const user = await this.users.findOne({ where: { email } });
+      if (!user) {
+        return { ok: false, error: 'User not found' };
+      }
+      const passwordCorrect = await user.checkPassword(password);
+      if (!passwordCorrect) {
+        return { ok: false, error: 'Wrong password' };
+      }
+      return { ok: true, token: 'test_token' };
+    } catch (error) {
+      return { ok: false, error };
+    }
+
   }
 }
