4dcaad9403ba6384581c18630c4de18452f5e89d
cchoseonghun <cchoseonghun@gmail.com>
2023-12-26 16:19:49 +0900

    nest-graphql2: Test - createAccount

--- nest-graphql2/src/users/users.service.spec.ts
+++ nest-graphql2/src/users/users.service.spec.ts
@@ -7,11 +7,21 @@ import { JwtService } from 'src/jwt/jwt.service';
 import { MailService } from 'src/mail/mail.service';
 import { Repository } from 'typeorm';
 
-const mockRepository = {
+// const mockRepository = {
+//   findOne: jest.fn(),
+//   save: jest.fn(), 
+//   create: jest.fn(), 
+// };
+// 단일 객체를 정의
+
+const mockRepository = () => ({
   findOne: jest.fn(),
   save: jest.fn(), 
   create: jest.fn(), 
-};
+});
+// 함수를 반환. 각 호출 시마다 새로운 객체를 생성하는데 
+// 각 테스트에서 별도의 인스턴스를 사용하여 테스트 간의 상태가 공유되지 않도록 하여 
+// 테스트의 독립성이 유지되어 예측 가능한 테스트 결과를 얻을 수 있음
 
 const mockJwtService = {
   sign: jest.fn(), 
@@ -27,6 +37,8 @@ type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 describe('UserService', () => {
   let service: UsersService;
   let usersRepository: MockRepository<User>;
+  let verificationsRepository: MockRepository<Verification>;
+  let mailService: MailService;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
@@ -34,11 +46,11 @@ describe('UserService', () => {
         UsersService, 
         {
           provide: getRepositoryToken(User),
-          useValue: mockRepository,
+          useValue: mockRepository(),
         },
         {
           provide: getRepositoryToken(Verification),
-          useValue: mockRepository,
+          useValue: mockRepository(),
         },
         {
           provide: JwtService,
@@ -52,6 +64,8 @@ describe('UserService', () => {
     }).compile();
     service = module.get<UsersService>(UsersService);
     usersRepository = module.get(getRepositoryToken(User));
+    verificationsRepository = module.get(getRepositoryToken(Verification));
+    mailService = module.get<MailService>(MailService);
   });
 
   it('should be defined', () => {
@@ -61,11 +75,43 @@ describe('UserService', () => {
   // it.todo('createAccount');
   describe('createAccount', () => {
     // it('should fail if user exists', () => {});
+    const createAccountArgs = {
+      email: 'bs@gmail.com', 
+      password: 'bs.password', 
+      role: 0, 
+    };
+    
     it('should fail if user exists', async () => {
       usersRepository.findOne.mockResolvedValue({ id: 1, email: '' });
-      
-      const result = await service.createAccount({ email: '', password: '', role: 0 });
+      const result = await service.createAccount(createAccountArgs);
+
       expect(result).toMatchObject({ ok: false, error: 'There is a user with that email already' });
+    });
+
+    it('should create a new user', async () => {
+      usersRepository.findOne.mockResolvedValue(undefined);
+      usersRepository.create.mockReturnValue(createAccountArgs);
+      usersRepository.save.mockResolvedValue(createAccountArgs);
+      
+      verificationsRepository.create.mockReturnValue({ user: createAccountArgs });
+      verificationsRepository.save.mockResolvedValue({ code: 'code' });
+
+      const result = await service.createAccount(createAccountArgs);
+
+      expect(usersRepository.create).toHaveBeenCalledTimes(1);
+      expect(usersRepository.create).toHaveBeenCalledWith(createAccountArgs);
+      expect(usersRepository.save).toHaveBeenCalledTimes(1);
+      expect(usersRepository.save).toHaveBeenCalledWith(createAccountArgs);
+
+      expect(verificationsRepository.create).toHaveBeenCalledTimes(1);
+      expect(verificationsRepository.create).toHaveBeenCalledWith({ user: createAccountArgs });
+      expect(verificationsRepository.save).toHaveBeenCalledTimes(1);
+      expect(verificationsRepository.save).toHaveBeenCalledWith({ user: createAccountArgs });
+
+      expect(mailService.sendVerificationEmail).toHaveBeenCalledTimes(1);
+      expect(mailService.sendVerificationEmail).toHaveBeenCalledWith(expect.any(String), expect.any(String));
+      
+      expect(result).toEqual({ ok: true });
     });
   });
   it.todo('login');
